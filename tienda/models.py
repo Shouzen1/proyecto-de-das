a603e1cf0c7ea35a75f49a7d7592dbf689efa436
xXAlex236Xx <eljugador1066@gmail.com>
2024-07-07 21:34:00 -0400

    Creacion de template, ajuste de modelo tiendas, creacion de vista y template crud_artista (por terminar) y creacion de base html

--- tienda/models.py
+++ tienda/models.py
@@ -4,24 +4,26 @@ from django.db import models
 from usuarios.models import Usuario
 
 
-class Obra(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    fecha = models.CharField(max_length=100)
-    precio = models.IntegerField()
 
-    def __str__(self):
-        return self.nombre 
-
-class Voto(models.Model):
-	obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
-	usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
 class Artista(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     fecha = models.CharField(max_length=100)
-    obras = models.ForeignKey(Obra,related_name='artista_obra', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre 
+
+class Obra(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    artista = models.ForeignKey(Artista,related_name='obras', on_delete=models.CASCADE)
+
+
 
     def __str__(self):
         return self.nombre
+class Voto(models.Model):
+	obra = models.ForeignKey(Obra,related_name='votos', on_delete=models.CASCADE)
+	usuario = models.ForeignKey(Usuario,related_name='votos', on_delete=models.CASCADE)
--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
+
+def crud_artista(request):
+
+	if request.POST:
+		print("POST")
+		if "obra_id" in request.POST:
+			print("La obra a editar es:", request.POST['obra_id'])
+
+
+
+		if "artista_id" in request.POST:
+			print("La autor a editar es:", request.POST['artista_id'])
+			artista_id = request.POST['artista_id']
+			artista = Artista.objects.get(id=artista_id)
+			
+			if 'Editar_artista' in request.POST:
+				print("Lo vamos a editar",artista)
+			if 'Eliminar_artista' in request.POST:
+				print("Lo vamos a eliminar",artista)
+
+	artistas = Artista.objects.all()
+	return render(request, 'tienda/crud_artista.html', {'artistas': artistas})
