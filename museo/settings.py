c43e84464ffacd30ff3227a0d8301a7fee148667
xXAlex236Xx <eljugador1066@gmail.com>
2024-07-10 09:59:22 -0400

    Añadicion modal,caruzel (sin terminar)

--- museo/settings.py
+++ museo/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'usuarios',
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
